9b38bc2b601253cff3ff87ae5946ddb1b9895fb9
youthhng <youthhng@gmail.com>
2021-01-18 23:26:55 +0800

    Create D01_Numpy.py

---
+++ D01_Numpy.py
@@ -0,0 +1,11 @@
+#1.生成一個等差數列，首數為0，尾數為20，公差為1的數列。
+import numpy as np
+print(np.arange(0,21))
+
+#2.呈上題，將以上數列取出偶數。
+import numpy as np
+print(np.arange(0,21,2))
+
+#3.呈1題，將數列取出3的倍數。
+import numpy as np
+print(np.arange(0,21,3))
