fd4a38b3059824ca95582752bd3290cf6ff2ee49
youthhng <youthhng@gmail.com>
2021-01-28 02:52:37 +0800

    Update D06_IO.py

--- D06_IO.py
+++ D06_IO.py
@@ -5,15 +5,15 @@ array1 = np.array(range(30))
 array2 = np.array([2,3,5])
 
 with open('file1.npz', 'wb') as f:
-    np.savez(f, a1=array1, a2=array2)
+    np.savez(f, array1, array2)
 
 
 # 2.讀取剛剛的 npz 檔，加入下列 array 一起存成新的 npz 檔
 array3 = np.array([[4,5,6], [1,2,3]])
 
 temp_file = np.load('file1.npz')
-t1 = temp_file['a1']
-t2 = temp_file['a2']
+t1 = temp_file['arr_0']
+t2 = temp_file['arr_1']
 
 with open('file2.npz', 'wb') as f:
-    np.savez(f, a1=t1, a2=t2, a3=array3[0], a4=array3[1])
+    np.savez(f, t1, t2, array3)
