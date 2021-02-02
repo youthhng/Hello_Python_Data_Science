62950473892ad78ad0b37b95d062130a88c5a633
youthhng <youthhng@gmail.com>
2021-02-03 02:05:17 +0800

    Update D11_Data_fillna.py

--- D11_Data_fillna.py
+++ D11_Data_fillna.py
@@ -19,8 +19,17 @@ q_df.fillna("others")
 
 
 #更進一步將字串做編碼。 此時用什麼方式做編碼比較適合?為什麼?
-from sklearn.preprocessing import LabelEncoder
+
 #Sex可用順序性or一般性
+from sklearn.preprocessing import LabelEncoder
 q_df['Sex_label'] = LabelEncoder().fit_transform(q_df['Sex']) 
+
 #Profession使用一般性
-q_df['Profession_label'] = LabelEncoder().fit_transform(q_df['Profession'].values)  #--> 讀取None導致錯誤
+getDummies = pd.get_dummies(q_df['Profession']) #Profession使用一般性
+pd.concat([q_df, getDummies], axis=1)
+
+#   Sex	    Profession	Sex_label	None	engineer	teacher
+#0	male	  teacher	    1	        0	    0	        1
+#1	male	  engineer	  1	        0	    1	        0
+#2	female	None	      0	        1	    0	        0
+#3	female	engineer	  0	        0	    1	        0
