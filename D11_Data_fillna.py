be23973fbd5d9233913c90754ae1f985f4e567f1
youthhng <youthhng@gmail.com>
2021-02-03 01:55:42 +0800

    Update D11_Data_fillna.py

--- D11_Data_fillna.py
+++ D11_Data_fillna.py
@@ -10,11 +10,17 @@ q_df = pd.DataFrame([['male', 'teacher'],
 #3	female	enginee
 
 #缺失值填入字串'others'
-
+q_df.fillna("others")
+#   Sex	    Profession
+#0	male	  teacher
+#1	male	  engineer
+#2	female	None   ----> 沒有被取代成"others"
+#3	female	enginee
 
 
 #更進一步將字串做編碼。 此時用什麼方式做編碼比較適合?為什麼?
 from sklearn.preprocessing import LabelEncoder
-q_df['Sex_label'] = LabelEncoder().fit_transform(q_df['Sex']) #Sex可用順序性or一般性
-
-q_df['Profession_label'] = LabelEncoder().fit_transform(q_df['Profession'].values) #Profession使用一般性
+#Sex可用順序性or一般性
+q_df['Sex_label'] = LabelEncoder().fit_transform(q_df['Sex']) 
+#Profession使用一般性
+q_df['Profession_label'] = LabelEncoder().fit_transform(q_df['Profession'].values)  #--> 讀取None導致錯誤
