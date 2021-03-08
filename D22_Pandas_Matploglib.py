39115fcfb0c20d429a8685834bc624e27bdc6306
youthhng <youthhng@gmail.com>
2021-03-08 23:18:40 +0800

    Create D22_Pandas_Matploglib.py

---
+++ D22_Pandas_Matploglib.py
@@ -0,0 +1,14 @@
+# 載入相關的程式庫
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 需要使用 Colab 請注意一下
+# 先行確認 Colab 上面的版本
+import keras
+print("keras:",keras.__version__)
+import tensorflow as tf
+print("tf:",tf.__version__)
